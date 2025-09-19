ba9dc5acb8b442b97a8f23fe8492b422faa6dbe1
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:07:26 +0100

    Update PS5_DataStructures_estruturas.c

--- PS5_DataStructures_estruturas.c
+++ PS5_DataStructures_estruturas.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+
+    head = (struct Node*) malloc(sizeof(struct Node));
+    second = (struct Node*) malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = NULL;
+
+    printf("Lista ligada: %d -> %d\n", head->data, second->data);
+
+    free(head);
+    free(second);
+    return 0;
+}
