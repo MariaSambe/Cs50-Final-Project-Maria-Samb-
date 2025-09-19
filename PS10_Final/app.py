4f508b27bee75997d2014d838daa1f614a03b62c
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:18:31 +0100

    Update app.py

--- PS10_Final/app.py
+++ PS10_Final/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template
 
+app = Flask(_name_)
+
+@app.route("/")
+def dashboard():
+    dados = {"imc": 22.5, "saldo": 150, "passos": 8500}
+    return render_template("dashboard.html", dados=dados)
+
+if _name_ == "_main_":
+    app.run(debug=True)
