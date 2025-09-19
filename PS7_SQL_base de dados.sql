ba125d8fc37b3a0bbb322f2c5ca6643e2c9f1532
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:08:34 +0100

    Update PS7_SQL_base de dados.sql

--- PS7_SQL_base de dados.sql
+++ PS7_SQL_base de dados.sql
@@ -1,1 +1,8 @@
+CREATE TABLE utilizadores (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    idade INTEGER
+);
 
+INSERT INTO utilizadores (nome, idade) VALUES ("Maria", 25);
+INSERT INTO utilizadores (nome, idade) VALUES ("João", 30);
