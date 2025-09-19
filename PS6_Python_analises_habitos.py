820a6e8c2d23d2b249eeea5a6f4cc0439df04cca
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:08:00 +0100

    Update PS6_Python_analises_habitos.py

--- PS6_Python_analises_habitos.py
+++ PS6_Python_analises_habitos.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+dados = {"Dia": ["Seg", "Ter", "Qua", "Qui", "Sex"],
+         "HorasEstudo": [2, 3, 1, 4, 2]}
+
+df = pd.DataFrame(dados)
+
+print("Média de estudo:", df["HorasEstudo"].mean())
+print("Dia com mais estudo:", df.loc[df["HorasEstudo"].idxmax(), "Dia"])
