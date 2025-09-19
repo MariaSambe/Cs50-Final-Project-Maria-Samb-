f07fe28f53c7c0373fc453ee38115b6cde581836
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:05:49 +0100

    Update PS2_Monitor_Passos.c

--- PS2_Monitor_Passos.c
+++ PS2_Monitor_Passos.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int meta, passos;
+    printf("Defina a meta de passos: ");
+    scanf("%d", &meta);
+
+    printf("Passos dados hoje: ");
+    scanf("%d", &passos);
+
+    printf("Progresso: %.2f%%\n", (passos * 100.0) / meta);
+
+    if(passos >= meta)
+        printf("Parabéns! Atingiu a meta.\n");
+    else
+        printf("Continue a caminhar!\n");
+
+    return 0;
+}
