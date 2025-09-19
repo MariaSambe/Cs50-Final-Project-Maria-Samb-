65566a1bdee7bc23ea68f165c3bf81aa3ccdc5ac
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:06:55 +0100

    Update PS4_Memory_memória.c

--- PS4_Memory_memória.c
+++ PS4_Memory_memória.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int n, *arr;
+
+    printf("Quantos números vai guardar? ");
+    scanf("%d", &n);
+
+    arr = (int*) malloc(n * sizeof(int));
+
+    for(int i = 0; i < n; i++) {
+        printf("Número %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Números guardados: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
