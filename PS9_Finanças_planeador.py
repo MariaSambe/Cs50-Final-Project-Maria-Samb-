9339c748dab52ac416074296c90ceaff4a61ae3c
MariaSambe <maria.data.analyst@gmail.com>
2025-09-19 17:10:28 +0100

    Update PS9_Finanças_planeador.py

--- PS9_Finanças_planeador.py
+++ PS9_Finanças_planeador.py
@@ -1,1 +1,8 @@
+despesas = []
+while True:
+    valor = input("Introduza uma despesa (ou 'sair'): ")
+    if valor == "sair":
+        break
+    despesas.append(float(valor))
 
+print("Total de despesas:", sum(despesas))
